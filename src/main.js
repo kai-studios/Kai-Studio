9fa95b1c2caa201aba39f91e2e0e3a29aca04ae4
kai studio <diekunnazar@gmail.com>
2025-01-09 16:56:09 +0200

    fix

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import "./javascript/ppage-slider"
-import "./javascript/slider"
-import "./javascript/ppage-slider"
-import "./javascript/slider"
