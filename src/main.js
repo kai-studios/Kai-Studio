8dc0dec2209e4dc2f7927d9980c85bb0ecf031e8
kai studio <diekunnazar@gmail.com>
2025-01-09 16:43:15 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import "./style/bproject.css"
-import "./style/contact.css"
-import "./style/footer.css"
-import "./style/PPage.css"
-import "./style/slider.css"
-import "./style/style.css"
+import "./style/bundler.css"
 import "./javascript/ppage-slider"
 import "./javascript/slider"
 import "./javascript/ppage-slider"
 import "./javascript/slider"
-
-document.querySelector('#app').innerHTML = ``
-
