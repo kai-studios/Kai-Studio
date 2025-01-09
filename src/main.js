28290fa1055fc33d080837865ec7b7f0ca037a07
kai studio <diekunnazar@gmail.com>
2025-01-09 16:47:13 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import "./style/bundler.css"
 import "./javascript/ppage-slider"
 import "./javascript/slider"
 import "./javascript/ppage-slider"
